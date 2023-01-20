19a903fc9ff27baba9dfc0ffb486962c126a17f1
ainiy09 <ainiy@yandex.ru>
2023-01-20 23:15:01 +0300

    landing

--- script/popup.js
+++ script/popup.js
@@ -1,29 +1,27 @@
 class Popup {
-    constructor(className) {
-      this._className = className;
-      this.popup = document.querySelector(`.${className}`);
-    }
-    open() {
-      this.popup.classList.add('popup_active');
-    }
-    close() {
-      this.popup.classList.remove('popup_active');
-    }
-    setEventListener() {
-      console.log(this.popup);
-      this.popup.addEventListener('click', (evt) => {
-        console.log(evt.target.classList);
-        console.log(evt.target.closest('.popup__close'));
-        if (
-          evt.target.classList.contains(this._className) ||
-          !!evt.target.closest('.popup__close')
-        ) {
-          this.close();
-        }
-      });
-    }
+  constructor(className) {
+    this._className = className;
+    this.popup = document.querySelector(`.${className}`);
+  }
+  open() {
+    this.popup.classList.add('popup__active');
+  }
+  close() {
+    this.popup.classList.remove('popup__active');
+  }
+  setEventListener() {
+    console.log(this.popup);
+    this.popup.addEventListener('click', (evt) => {
+      console.log(evt.target.closest('.popup__close'));
+      if (
+        evt.target.classList.contains(this._className) ||
+        !!evt.target.closest('.popup__close')
+      ) {
+        this.close();
+      }
+    });
+  }
 }
-  
+
 const popups = new Popup('popup-add-cats');
 console.log(popups);
-  
